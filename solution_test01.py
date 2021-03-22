a33798d6b44fe9169b989c255b203fa6ef28499e
동건 <dong@dong-geon-ui-MacBookPro.local>
2021-03-22 19:45:23 +0900

    source code

---
+++ solution_test01.py
@@ -0,0 +1,10 @@
+from solution01 import solution
+
+arr = [1, 1, 3, 3, 0, 1, 1]
+print(solution(arr))
+
+arr = [4, 4, 4, 3, 3]
+print(solution(arr))
+
+arr = [1, 3, 4, 4, 5, 3, 3, 1]
+print(solution(arr))
